9990fb86a1b128a3750da13ff62ba8c42802fe10
Mustafa Khamis <safsafrwanda2006@gmail.com>
2026-03-23 14:41:33 +0200

    Update MedicinesTable.jsx

--- src/pages/MedicinesTable.jsx
+++ src/pages/MedicinesTable.jsx
@@ -190,7 +190,7 @@ function MedicinesTable({ language = "en" }) {
                 <button
                   type="button"
                   className="ended-btn"
-                  onClick={() => updateStatus(m.id, "finished")}
+                  onClick={() => updateStatus(m.id, "unavailable")}
                 >
                   {t("outOfStock")}
                 </button>
